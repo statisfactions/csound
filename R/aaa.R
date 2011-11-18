7841151745cf239860b45403f83f33820b42edb1
Ethan Brown <ethancbrown@gmail.com>
2011-11-18 10:20:53 -0500

    boosted range of dll search

--- R/aaa.R
+++ R/aaa.R
@@ -1,12 +1,19 @@
 ##' @import rdyncall
 
 .onLoad <- function(libname, pkgname) {
-  if(is.null(getOption("csoundlibrary")))
-    csndlib <- dynfind(c("csound64",
-                         "csound64.dll.5.2",
-                         "csound.dll.5.2",
-                         
-                         "lib_csnd64.dylib"))
+  if(is.null(getOption("csoundlibrary"))) {
+    candidates <- c("csound64",
+                    "csnd64",
+                    "csound",
+                    "csnd",
+                    "lib_csnd64.dylib",
+                    "csound64.dll",
+                    "csound.dll")
+    candidates <- c(candidates,
+                    paste(candidates, 5, formatC(40:1, width = 2, flag = "0"), sep = "."),
+                    paste(candidates, 5, 9:1, sep = "."))
+    csndlib <- dynfind()
+  }
   if(is.null(csndlib)) {
     packageStartupMessage("Note: No valid csound library found automatically. You must have a valid Csound library to use this package; if you know the path to your Csound shared library, you can specify it using setCsoundLibrary.")
   } else options(csoundlibrary=csndlib)
