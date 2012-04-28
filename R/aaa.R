c6928316f7d73e4b99db1c5e8e49997718797b7e
Ethan Brown <ethancbrown@gmail.com>
2012-04-28 16:07:32 -0400

    silly typo in aaa.R

--- R/aaa.R
+++ R/aaa.R
@@ -9,13 +9,20 @@
                     "lib_csnd64.dylib",
                     "csound64.dll",
                     "csound.dll")
-    candidates <- c(candidates,
+    candidatesfull <- c(candidates,
                     unlist(sapply(candidates, function(x)
                                   paste(x, 5, formatC(40:1, width = 2, flag = "0"), sep = "."))),
                     unlist(sapply(candidates, function(x)
                                   paste(x, 5, 9:1, sep = "."))))
-    candidates <- setdiff(candidates, c("csnd", "csound", "csound.dll"))
-    csndlib <- dynfind(candidates)
+    candidatesfiltered <- setdiff(candidatesfull, c("csnd", "csound", "csound.dll"))
+    csndlib <- dynfind(candidatesfull)
+    if(!is.null(csndlib) && (basename(attr(csndlib, "path")) %in% c("csnd", "csnd.dll"))) {
+      path <- dirname(attr(csndlib, "path"))
+      pathlist <- dir(path)
+      matches <- pathlist %in% candidatesfiltered
+      if(any(matches))
+        csndlib <- .dynload(dir(path, full.names = T)[which(matches)[1]])
+    }
   }
   if(is.null(csndlib)) {
     packageStartupMessage("Note: No valid csound library found automatically. You must have a valid Csound library to use this package; if you know the path to your Csound shared library, you can specify it using setCsoundLibrary.")
